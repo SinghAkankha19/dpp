c55ca73de529974e706d1f453ba6561293b1f38b
SinghAkankha19 <159901688+SinghAkankha19@users.noreply.github.com>
2025-04-15 02:52:14 +0530

    Update main.tf

--- app-gateway/main.tf
+++ app-gateway/main.tf
@@ -1,8 +1,4 @@
-resource "azurerm_resource_group" "example" {
-  name     = "appgateway"
-  location = "West Europe"
 
-}
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
