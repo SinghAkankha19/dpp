0a80e6c9d7ca29767f3adecfadb9253dcec48550
SinghAkankha19 <159901688+SinghAkankha19@users.noreply.github.com>
2025-04-15 02:34:34 +0530

    Update provider.tf

--- app-gateway/provider.tf
+++ app-gateway/provider.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.26.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
     }
   }
+
+  required_version = ">= 1.3.0"
 }
 
 provider "azurerm" {
- features {}
+  features {}
 }
