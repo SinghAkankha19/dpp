e09b584932f22c9154db5a21f64b83c022106372
SinghAkankha19 <159901688+SinghAkankha19@users.noreply.github.com>
2025-07-12 12:48:27 +0530

    Update resource.tf

--- app-gateway/resource.tf
+++ app-gateway/resource.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "app-gtw"
-  location = "West Europe"
-}
+#resource "azurerm_resource_group" "rg" {
+ # name     = "app-gtw"
+ # location = "West Europe"
+#}
