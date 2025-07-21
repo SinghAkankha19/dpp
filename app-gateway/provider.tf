c9039c8c59ade0b65f0c19f9fd05967038d4ab4b
SinghAkankha19 <159901688+SinghAkankha19@users.noreply.github.com>
2025-07-21 21:57:20 +0530

    Update provider.tf

--- app-gateway/provider.tf
+++ app-gateway/provider.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "6d469494-cb9b-4dbb-99d2-e4d17b4d560e"
+  subscription_id = "3c6a135b-2614-404b-a1d6-a84cdfc6a2d7"
  # client_id       = "0282f4c3-8269-44d6-8178-ca85ed922656"
  # client_secret   = "QY98Q~AXIhfdLtssNr1IUDM5UCzE03R5ctsuvbC."
  # tenant_id       = "1a1bf354-0f14-4afe-8e0f-911dd910688a"
